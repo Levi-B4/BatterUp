41f75279abf1beddc4fade0770b4dec28231154a
Levi-B4 <levi.butler4@gmail.com>
2023-07-13 13:20:50 -0400

    Created window layout

--- BatterUpGUI.java
+++ BatterUpGUI.java
@@ -1,11 +1,61 @@
 import java.io.IOException;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+import java.awt.FlowLayout;
 
 public class BatterUpGUI extends JFrame{
-    BatterUp game = new BatterUp();
+    //Logic
+    private BatterUp game = new BatterUp();
+    
+    //GUI Components
+    private JLabel inningsLabel;
+    private JTextField inningsTextField;
+
+    private JLabel teamNameLabel;
+    private JTextField teamNameTextField;
+
+    private JButton startButton;
+    private JButton resetButton;
+
+    private JTextArea gameOutput;
+    private JScrollPane scrollPane;
+
     
     public BatterUpGUI() throws IOException{
+        setTitle("Batter up!");
+
+        setLayout(new FlowLayout());
+        
+        //set components
+        inningsLabel = new JLabel("Number of Innings");
+        inningsTextField = new JTextField(20);
+
+        teamNameLabel = new JLabel("Team Name");
+        teamNameTextField = new JTextField(20);
+
+        startButton = new JButton("Start");
+        resetButton = new JButton("Reset");
+
+        //setup output
+        gameOutput = new JTextArea(20,50);
+        scrollPane = new JScrollPane(gameOutput);
+        gameOutput.setEditable(false);
+
+        //add components to container
+        add(inningsLabel);
+        add(inningsTextField);
+        add(teamNameLabel);
+        add(teamNameTextField);
+        add(startButton);
+        add(resetButton);
+        add(scrollPane);
+
         game.Play(9);
     }
 }
--- GUILauncher.java
+++ GUILauncher.java
@@ -1,11 +1,13 @@
 import java.io.IOException;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class GUILauncher{
     public static void main(String[] args) throws IOException{
         BatterUpGUI theGame = new BatterUpGUI();
         theGame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        theGame.setSize(650,450); 
+        theGame.setSize(650,450);
+        theGame.setResizable(false);
         theGame.setVisible(true);
     }
 }
